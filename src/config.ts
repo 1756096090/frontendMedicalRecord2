ff022792d8c4c822dcaccaa8575ac95d0d3eab68
1756096090 <84259949+1756096090@users.noreply.github.com>
2024-11-04 22:03:58 -0500

    https

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: 'backendmedicalrecord-production.up.railway.app'
+    API_BASE_URL: 'https://backendmedicalrecord-production.up.railway.app'
 };
 
 export default config;
